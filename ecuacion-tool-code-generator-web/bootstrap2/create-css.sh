ae27ea117dd009bf78479f1d6cc6d9b139e857eb
Yosuke Tanaka <yosuke.tanaka@ecuacion.jp>
2025-07-27 10:32:51 +0900

    Metafiles updated.

--- ecuacion-tool-code-generator-web/bootstrap2/create-css.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-export PROJ_NAME=ecuacion-tool-code-generator/ecuacion-tool-code-generator-web
-
-../../../ecuacion-internal-utils/ecuacion-util-bootstrap/script/command.sh `pwd`/custom.scss
-
-mv ../../../ecuacion-internal-utils/ecuacion-util-bootstrap/script/bootstrap* .
-cp bootstrap* ../../../${PROJ_NAME}/src/main/resources/static/css/
-rm bootstrap*
