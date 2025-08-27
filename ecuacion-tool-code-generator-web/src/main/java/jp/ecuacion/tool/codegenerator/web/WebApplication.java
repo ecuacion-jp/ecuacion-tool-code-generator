5378daf5297d8028e4e9aaa6763a19aa31270762
Yosuke Tanaka <yosuke.tanaka@ecuacion.jp>
2025-08-28 06:40:11 +0900

    ecuacion-modules updated.

--- ecuacion-tool-code-generator-web/src/main/java/jp/ecuacion/tool/codegenerator/web/WebApplication.java
+++ ecuacion-tool-code-generator-web/src/main/java/jp/ecuacion/tool/codegenerator/web/WebApplication.java
@@ -1,11 +1,9 @@
 package jp.ecuacion.tool.codegenerator.web;
 
-import jp.ecuacion.splib.web.bean.SplibModelAttributes;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class WebApplication extends SpringBootServletInitializer {
@@ -17,15 +15,5 @@ public class WebApplication extends SpringBootServletInitializer {
   @Override
   protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
     return application.sources(WebApplication.class);
-  }
-  
-  @Bean
-  SplibModelAttributes appCommonModelAttributes() {
-    SplibModelAttributes atr = new SplibModelAttributes();
-    atr.setBsBgGradient(true);
-    atr.setShowsMessagesLinkedToItemsAtTheTop(true);
-    atr.setShowsMessagesLinkedToItemsAtEachItem(false);
-    
-    return atr;
   }
 }
