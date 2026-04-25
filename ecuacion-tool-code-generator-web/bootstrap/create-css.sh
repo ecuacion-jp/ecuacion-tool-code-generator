b8854b24f85e855530ae0212f58c17e51220f94b
Yosuke Tanaka <yosuke.tanaka@ecuacion.jp>
2026-04-26 05:07:45 +0900

    ecuacion-lib update

--- ecuacion-tool-code-generator-web/bootstrap/create-css.sh
+++ ecuacion-tool-code-generator-web/bootstrap/create-css.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 export app_script_path=$(cd $(dirname $0); pwd)
-${app_script_path}/../../../ecuacion-internal-utils/ecuacion-util-bootstrap/script/command.sh 
+${app_script_path}/../../../ecuacion-build-tools/ecuacion-util-bootstrap/script/command.sh 
